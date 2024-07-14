a6b357a84b6744e5ad29aa86ddab25605c461b0f
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-07-14 20:42:18 -0300

    a

--- Engenharia.py
+++ Engenharia.py
@@ -13,7 +13,7 @@ tab1, tab2 = st.tabs(["Custos","Projeto"])
 
 #----------------------------------------------------------------------------------------------------
 #dados
-link_projeto = "https://sketchfab.com/3d-models/5772-7636e807e8994849af58b1078c83bb66/embed"
+link_projeto = "https://sketchfab.com/3d-models/1374-3d-residencia-mk-22-11-23-ifc-e275718ed9be41f9bf28824b8cc924f8/embed"
 link_planilha = "https://docs.google.com/spreadsheets/d/e/2PACX-1vST4hynvdLNKaV0sVyg_vTtLGQmCnPPRgq8-TEztlYhG_rqVtlCjlOEbekBOufGVbbJDI5FMmJ6zhFZ/pub?output=csv"
 
 df = pd.read_csv(link_planilha)
